48fe0477ae31f125249dab939c33884eb3049263
WMonach <mimimonach@gmail.com>
2023-01-31 10:08:48 +0100

    abort

--- hday1/ex03/Weapon.cpp
+++ hday1/ex03/Weapon.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Weapon.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wmonacho <wmonacho@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: will <will@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 18:56:40 by wmonacho          #+#    #+#             */
-/*   Updated: 2023/01/26 18:57:59 by wmonacho         ###   ########lyon.fr   */
+/*   Updated: 2023/01/27 19:55:27 by will             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ Weapon::Weapon(void)
 
 Weapon::~Weapon(void)
 {
+}
+
+Weapon::Weapon(std::string type)
+{
+	this->_type = type;
 }
 
 Weapon	Weapon::getType(void)
--- hday1/ex03/Weapon.hpp
+++ hday1/ex03/Weapon.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Weapon.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wmonacho <wmonacho@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: will <will@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 17:18:37 by wmonacho          #+#    #+#             */
-/*   Updated: 2023/01/26 17:27:18 by wmonacho         ###   ########lyon.fr   */
+/*   Updated: 2023/01/27 19:56:18 by will             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@
 class Weapon
 {
 	private :
-			std::string	type;
+			std::string	_type;
 
 	public :
 			Weapon(void);
+			Weapon(std::string type);
 			~Weapon(void);
 			Weapon	getType(void);
 			void	setType(Weapon new_type);
