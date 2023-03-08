68548f7c646a03228ef39de034cec9f6ba931e13
WMonach <mimimonach@gmail.com>
2023-03-08 14:04:02 +0100

    all piscine CPP finish

--- hday8/ex01/Span.hpp
+++ hday8/ex01/Span.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: will <will@student.42lyon.fr>              +#+  +:+       +#+        */
+/*   By: wmonacho <wmonacho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 19:34:31 by will              #+#    #+#             */
-/*   Updated: 2023/03/07 23:40:28 by will             ###   ########lyon.fr   */
+/*   Updated: 2023/03/08 13:45:49 by wmonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,26 @@
 
 class Span
 {
+	public :
+        class SizeToLow : public std::exception
+        {
+            public :
+                virtual const char* what() const throw()
+                {
+                    return ("Size to low");
+                }
+        };  
+        
+        class RangeIteratorToBig : public std::exception
+        {
+            public :
+                virtual const char* what() const throw()
+                {
+                    return ("Range iterator is bigger than size max");
+                }
+        };
 		private :
-		unsigned int					_SizeMax;
+		unsigned int							_SizeMax;
 		std::vector<unsigned int>				_Containers;
 		
 		public :
