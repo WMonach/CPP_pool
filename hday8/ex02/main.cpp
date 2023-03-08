3e4c1855509badb28b7b4a05cfe0cca96e5c3677
WMonach <mimimonach@gmail.com>
2023-03-08 14:35:47 +0100

    wfsf

--- hday8/ex02/main.cpp
+++ hday8/ex02/main.cpp
@@ -6,88 +6,88 @@
 /*   By: wmonacho <wmonacho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 16:44:24 by wmonacho          #+#    #+#             */
-/*   Updated: 2023/03/08 13:57:46 by wmonacho         ###   ########.fr       */
+/*   Updated: 2023/03/08 14:34:23 by wmonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
-int main()
-{
-    std::cout << std::endl;
-    std::cout << "########### Mutant stack ###########" << std::endl;
-    std::cout << std::endl;
-    {
-        MutantStack<int> mstack;
-        mstack.push(5);
-        mstack.push(17);
-        std::cout << mstack.top() << std::endl;
-        mstack.pop();
-        std::cout << mstack.size() << std::endl;
-        mstack.push(3);
-        mstack.push(5);
-        mstack.push(737);
-        mstack.push(0);
+// int main()
+// {
+//     std::cout << std::endl;
+//     std::cout << "########### Mutant stack ###########" << std::endl;
+//     std::cout << std::endl;
+//     {
+//         MutantStack<int> mstack;
+//         mstack.push(5);
+//         mstack.push(17);
+//         std::cout << mstack.top() << std::endl;
+//         mstack.pop();
+//         std::cout << mstack.size() << std::endl;
+//         mstack.push(3);
+//         mstack.push(5);
+//         mstack.push(737);
+//         mstack.push(0);
 
-        MutantStack<int>::iterator it = mstack.begin();
-        MutantStack<int>::iterator ite = mstack.end();
-        ++it;
-        --it;
-        while (it != ite)
-        {
-            std::cout << *it << std::endl;
-            ++it;
-        }
-        std::stack<int> s(mstack);
-    }
-    std::cout << std::endl;
-    std::cout << "########### Mutant stack with random numbers ###########" << std::endl;
-    std::cout << std::endl;
-    {
-        MutantStack<int> mstack;
-        srand(time(0));
-        for (int i = 0; i < 10; i++)
-        {
-            int nb = rand()%100 + 1;
-            mstack.push(nb);
-        }
-        MutantStack<int>::iterator it = mstack.begin();
-        MutantStack<int>::iterator ite = mstack.end();
-        ++it;
-        --it;
-        while (it != ite)
-        {
-            std::cout << *it << std::endl;
-            ++it;
-        }
-        std::stack<int> s(mstack);
-    }
-    std::cout << std::endl;
-    std::cout << "########### List container ###########" << std::endl;
-    std::cout << std::endl;
-    {
-        std::list<int> mstack;
-        mstack.push_back(5);
-        mstack.push_back(17);
-        std::cout << mstack.size() << std::endl;
-        mstack.push_back(3);
-        mstack.push_back(5);
-        mstack.push_back(737);
-        mstack.push_back(0);
+//         MutantStack<int>::iterator it = mstack.begin();
+//         MutantStack<int>::iterator ite = mstack.end();
+//         ++it;
+//         --it;
+//         while (it != ite)
+//         {
+//             std::cout << *it << std::endl;
+//             ++it;
+//         }
+//         std::stack<int> s(mstack);
+//     }
+//     std::cout << std::endl;
+//     std::cout << "########### Mutant stack with random numbers ###########" << std::endl;
+//     std::cout << std::endl;
+//     {
+//         MutantStack<int> mstack;
+//         srand(time(0));
+//         for (int i = 0; i < 10; i++)
+//         {
+//             int nb = rand()%100 + 1;
+//             mstack.push(nb);
+//         }
+//         MutantStack<int>::iterator it = mstack.begin();
+//         MutantStack<int>::iterator ite = mstack.end();
+//         ++it;
+//         --it;
+//         while (it != ite)
+//         {
+//             std::cout << *it << std::endl;
+//             ++it;
+//         }
+//         std::stack<int> s(mstack);
+//     }
+//     std::cout << std::endl;
+//     std::cout << "########### List container ###########" << std::endl;
+//     std::cout << std::endl;
+//     {
+//         std::list<int> mstack;
+//         mstack.push_back(5);
+//         mstack.push_back(17);
+//         std::cout << mstack.size() << std::endl;
+//         mstack.push_back(3);
+//         mstack.push_back(5);
+//         mstack.push_back(737);
+//         mstack.push_back(0);
 
-        std::list<int>::iterator it = mstack.begin();
-        std::list<int>::iterator ite = mstack.end();
-        ++it;
-        --it;
-        while (it != ite)
-        {
-            std::cout << *it << std::endl;
-            ++it;
-        }
-        std::list<int> s(mstack);
-    }
-	return (0);
-}
+//         std::list<int>::iterator it = mstack.begin();
+//         std::list<int>::iterator ite = mstack.end();
+//         ++it;
+//         --it;
+//         while (it != ite)
+//         {
+//             std::cout << *it << std::endl;
+//             ++it;
+//         }
+//         std::list<int> s(mstack);
+//     }
+// 	return (0);
+// }
 
 // int main()
 // {
@@ -114,3 +114,28 @@ int main()
 //     std::stack<int> s(mstack);
 //     return (0);
 // }
+
+int main()
+{
+    std::list<int> list;
+    list.push_back(5);
+    list.push_back(17);
+    std::cout << list.back() << std::endl;
+    list.pop_back();
+    std::cout << list.size() << std::endl;
+    list.push_back(3);
+    list.push_back(5);
+    list.push_back(737);
+    //[...]
+    list.push_back(0);
+    std::list<int>::iterator it = list.begin();
+    std::list<int>::iterator ite = list.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    return (0);
+}
