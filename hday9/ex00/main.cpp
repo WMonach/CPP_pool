b115e77e1ac858e6382a59ca9de3832754eee150
WMonach <mimimonach@gmail.com>
2023-03-21 15:35:36 +0100

    cpp9 free

---
+++ hday9/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: will <will@student.42lyon.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 15:17:41 by will              #+#    #+#             */
+/*   Updated: 2023/03/21 15:32:41 by will             ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+#include <iostream>
+
+void parsing(string *filename)
+{
+	
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		parsing(argc, argv[1]);
+	}
+	return (0);
+}
